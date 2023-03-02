631e63c7be5651f2c30e54eabbfe0e6ddd5440ae
Walter Celiz <wceliz.dev@gmail.com>
2023-03-02 11:53:09 -0300

    feat: first commit

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hola Chango");
+console.log("Hola Walter");
+// esto es otro comentario
+/* esto es un comentario */
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("Hola Chango");
+console.log("Hola Walter");
+
+// esto es otro comentario
+/* esto es un comentario */
